8cb32b219fb113c454b508ca560d8f1468fb2e5e
mveysey <71096754+mveysey@users.noreply.github.com>
2021-04-30 20:57:20 -0400

    Second

---
+++ AccessiLearn VR/Assets/Scripts/DialogueManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueManager : MonoBehaviour
+{
+    public static DialogueManager Instance { get; private set; }
+    private AudioSource audio;
+
+
+    void Awake()
+    {
+        //Destroys the second instance of the subtitles 
+        //To destroy the first instance use Intstance == this
+        if(Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        Instance = this;
+        audio = gameObject.AddComponent<AudioSource>();
+    }
+
+    public void BeginDialogue(AudioClip passedClip)
+    {
+
+    }
+
+}
