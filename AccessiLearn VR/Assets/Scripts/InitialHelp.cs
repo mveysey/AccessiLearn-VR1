db2893a47e4bd056fe7efb8e1f077c0bce6c5016
mveysey <71096754+mveysey@users.noreply.github.com>
2021-05-01 02:00:53 -0400

    Three

---
+++ AccessiLearn VR/Assets/Scripts/InitialHelp.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class InitialHelp : MonoBehaviour
+{
+    public Button yourButton;
+    public GameObject box1;
+    public GameObject box2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button btn = yourButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+
+    }
+
+    // Update is called once per frame
+    void TaskOnClick()
+    {
+        box1.SetActive(false);
+        box2.SetActive(false);
+
+    }
+}
---
+++ AccessiLearn VR/Assets/Scripts/NewDialogueManager.cs
@@ -0,0 +1,197 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewDialogueManager : MonoBehaviour
+{
+    public Text dialogueDisplay;
+
+    public AudioClip S1;
+    public AudioClip S2;
+    public AudioClip S3;
+    public AudioClip S4;
+    public AudioClip S5;
+    public AudioClip S6;
+    public AudioClip S7;
+    public AudioClip S8;
+    AudioSource audioSource;
+
+   
+
+    [TextArea(3, 30)]
+    public string[] sentences;
+    private int index;
+    private int audioNumber = 0;
+
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if (!audioSource.isPlaying && audioNumber == 1)
+        {
+            Debug.Log("hi");
+            index++;
+            audioNumber++;
+
+            dialogueDisplay.text = sentences[1];
+            audioSource.clip = S2;
+            audioSource.Play();
+
+            Debug.Log("bye");
+        }
+
+        if (!audioSource.isPlaying && audioNumber == 2)
+        {
+            index++;
+            audioNumber++;
+
+            dialogueDisplay.text = sentences[2];
+            audioSource.clip = S3;
+            audioSource.Play();
+        }
+
+        if (!audioSource.isPlaying && audioNumber == 3)
+        {
+            index++;
+            audioNumber++;
+
+            dialogueDisplay.text = sentences[3];
+            audioSource.clip = S4;
+            audioSource.Play();
+        }
+
+        if (!audioSource.isPlaying && audioNumber == 4)
+        {
+            index++;
+            audioNumber++;
+
+            dialogueDisplay.text = sentences[4];
+            audioSource.clip = S5;
+            audioSource.Play();
+        }
+
+        if (!audioSource.isPlaying && audioNumber == 5)
+        {
+            index++;
+            audioNumber++;
+
+            dialogueDisplay.text = sentences[5];
+            audioSource.clip = S6;
+            audioSource.Play();
+        }
+        if (!audioSource.isPlaying && audioNumber == 6)
+        {
+            index++;
+            audioNumber++;
+
+            dialogueDisplay.text = sentences[6];
+            audioSource.clip = S7;
+            audioSource.Play();
+        }
+        if (!audioSource.isPlaying && audioNumber == 7)
+        {
+            index++;
+            audioNumber++;
+
+            dialogueDisplay.text = sentences[7];
+            audioSource.clip = S8;
+            audioSource.Play();
+        }
+    }
+
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "object")
+        {
+            audioNumber++;
+            dialogueDisplay.text = sentences[0];
+            audioSource.clip = S1;
+            audioSource.Play();
+            Destroy(other.gameObject);
+
+        }
+    }
+    
+
+    
+
+    public void NextSentence()
+    {
+
+        if (!audioSource.isPlaying && audioNumber == 1)
+        {
+            Debug.Log("hi");
+            index++;
+            audioNumber++;
+
+            dialogueDisplay.text = sentences[1];
+            audioSource.clip = S2;
+            audioSource.Play();
+
+            Debug.Log("bye");
+        }
+
+        if (!audioSource.isPlaying && audioNumber == 2)
+        {
+                index++;
+                audioNumber++;
+
+                dialogueDisplay.text = sentences[2];
+                audioSource.clip = S3;
+                audioSource.Play();
+        }
+
+            if (!audioSource.isPlaying && audioNumber == 3)
+            {
+            index++;
+            audioNumber++;
+
+            dialogueDisplay.text = sentences[3];
+            audioSource.clip = S4;
+            audioSource.Play();
+        }
+
+            if (!audioSource.isPlaying && audioNumber == 4)
+            {
+            index++;
+            audioNumber++;
+
+            dialogueDisplay.text = sentences[4];
+            audioSource.clip = S5;
+            audioSource.Play();
+        }
+
+            if (!audioSource.isPlaying && audioNumber == 5)
+            {
+            index++;
+            audioNumber++;
+
+            dialogueDisplay.text = sentences[5];
+            audioSource.clip = S6;
+            audioSource.Play();
+        }
+            if (!audioSource.isPlaying && audioNumber == 6)
+            {
+            index++;
+            audioNumber++;
+
+            dialogueDisplay.text = sentences[6];
+            audioSource.clip = S7;
+            audioSource.Play();
+        }
+            if (!audioSource.isPlaying && audioNumber == 7)
+            {
+            index++;
+            audioNumber++;
+
+            dialogueDisplay.text = sentences[7];
+            audioSource.clip = S8;
+            audioSource.Play();
+        }
+    }
+}
